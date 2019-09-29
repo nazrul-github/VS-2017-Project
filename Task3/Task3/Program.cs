994847d511d25d70b22e0e1d0199f07d82a8e763
Nazrul <nazrulgithub@gmail.com>
2019-09-30 00:29:23 +0600

    added task3

---
+++ Task3/Task3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter width and height of your monitor to check for landscape or potrait");
+            Console.WriteLine("Please enter the width");
+            int width = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter the height");
+            int height = Convert.ToInt32( Console.ReadLine());
+
+            if(width>height){
+                Console.WriteLine("This image is a landscape");
+                }else{Console.WriteLine("This image is a potrait");}
+             
+        }
+    }
+}
