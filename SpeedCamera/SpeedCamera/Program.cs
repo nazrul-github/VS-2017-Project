25134cdc287eef6857244334c245b350771b4311
Nazrul <nazrulgithub@gmail.com>
2019-09-30 09:59:48 +0600

    Your job is to write a program for a speed camera. For simplicity, ignore the details such as camera, sensors, etc and focus purely on the logic. Write a program that asks the user to enter the speed limit. Once set, the program asks for the speed of a car. If the user enters a value less than the speed limit, program should display Ok on the console. If the value is above the speed limit, the program should calculate the number of demerit points. For every 5km/hr above the speed limit, 1 demerit points should be incurred and displayed on the console. If the number of demerit points is above 12, the program should display License Suspended.

---
+++ SpeedCamera/SpeedCamera/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpeedCamera
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the speed limit");
+            int speedLimit  = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter the speed of a vehicle: ");
+            int vehicleSpeed = Convert.ToInt32(Console.ReadLine());
+            if (vehicleSpeed < speedLimit)
+            {
+                Console.WriteLine("Ok");
+            }else if(vehicleSpeed>speedLimit){
+                int differenceInSpeed = vehicleSpeed - speedLimit;
+                int demeritPoints = differenceInSpeed / 5;
+                if (demeritPoints > 12)
+                {
+                    Console.WriteLine("License Suspended");
+                }
+                else { Console.WriteLine("{0} demerit points added to the license",demeritPoints); }
+            }
+            else { Console.WriteLine("Please enter a correct vehicle speed"); }
+            
+        }
+    }
+}
