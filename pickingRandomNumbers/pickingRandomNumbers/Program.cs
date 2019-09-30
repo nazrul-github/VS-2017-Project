8f695e996f47de6ae693a9ceb27b914530cea2b3
Nazrul <nazrulgithub@gmail.com>
2019-09-30 19:04:26 +0600

    Write a program that picks a random number between 1 and 10. Give the user 4 chances to guess the number. If the user guesses the number, display “You won"; otherwise, display “You lost". (To make sure the program is behaving correctly, you can display the secret number on the console first.)

---
+++ pickingRandomNumbers/pickingRandomNumbers/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pickingRandomNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var randomNumber = new Random();
+            int randomNumberForGuess = randomNumber.Next(1,10);
+            Console.WriteLine($"Your Random Number Is: {randomNumberForGuess}");
+            for (int i = 0; i < 4; i++)
+            {
+                Console.WriteLine("Gues the number");
+                int guessTheNumber = Convert.ToInt32( Console.ReadLine());
+                if(guessTheNumber == randomNumberForGuess)
+                {
+                    Console.WriteLine("You Won");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("You Lost");
+                }
+
+            }
+        }
+    }
+}
