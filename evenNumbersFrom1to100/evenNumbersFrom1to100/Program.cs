2aaddc3c45617687852ec251a8db5c19d38d5bee
Nazrul <nazrulgithub@gmail.com>
2019-10-16 20:56:59 +0600

    Practiced printing even numbers to 100 for Console App
    `

---
+++ evenNumbersFrom1to100/evenNumbersFrom1to100/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace evenNumbersFrom1to100
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 1; i<=100; i++)
+            {
+                if(i%2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
