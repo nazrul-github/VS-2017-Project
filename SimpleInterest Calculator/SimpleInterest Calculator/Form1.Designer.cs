406d1b8808b83f393e629708d6a862466dd5ff03
Nazrul <nazrulgithub@gmail.com>
2019-10-16 21:02:15 +0600

    Simple Interest Calculator with my own design and code

---
+++ SimpleInterest Calculator/SimpleInterest Calculator/Form1.Designer.cs
@@ -0,0 +1,297 @@
+﻿namespace SimpleInterest_Calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.perYearLabel = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.timeSelectionComboBox = new System.Windows.Forms.ComboBox();
+            this.resultDisplayLabel = new System.Windows.Forms.Label();
+            this.thirdLabel = new System.Windows.Forms.Label();
+            this.secondLabel = new System.Windows.Forms.Label();
+            this.firstLabel = new System.Windows.Forms.Label();
+            this.calculateButton = new System.Windows.Forms.Button();
+            this.thirdTextBox = new System.Windows.Forms.TextBox();
+            this.secondTextBox = new System.Windows.Forms.TextBox();
+            this.firstTextBox = new System.Windows.Forms.TextBox();
+            this.whereConditionLabel = new System.Windows.Forms.Label();
+            this.solveLabel = new System.Windows.Forms.Label();
+            this.conditionSelectionComboBox = new System.Windows.Forms.ComboBox();
+            this.perYearLastLabel = new System.Windows.Forms.Label();
+            this.resultLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // perYearLabel
+            // 
+            this.perYearLabel.AutoSize = true;
+            this.perYearLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.perYearLabel.ForeColor = System.Drawing.Color.Gray;
+            this.perYearLabel.Location = new System.Drawing.Point(157, 232);
+            this.perYearLabel.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.perYearLabel.Name = "perYearLabel";
+            this.perYearLabel.Size = new System.Drawing.Size(70, 20);
+            this.perYearLabel.TabIndex = 33;
+            this.perYearLabel.Text = "per year";
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.Maroon;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(96, 9);
+            this.label2.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(446, 52);
+            this.label2.TabIndex = 31;
+            this.label2.Text = "Simple Interest Calculator";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // clearButton
+            // 
+            this.clearButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.clearButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.clearButton.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.clearButton.Location = new System.Drawing.Point(101, 375);
+            this.clearButton.Margin = new System.Windows.Forms.Padding(6);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(86, 42);
+            this.clearButton.TabIndex = 25;
+            this.clearButton.Text = "Clear";
+            this.clearButton.UseVisualStyleBackColor = false;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+            // 
+            // timeSelectionComboBox
+            // 
+            this.timeSelectionComboBox.BackColor = System.Drawing.Color.White;
+            this.timeSelectionComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.timeSelectionComboBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.timeSelectionComboBox.FormattingEnabled = true;
+            this.timeSelectionComboBox.Location = new System.Drawing.Point(223, 305);
+            this.timeSelectionComboBox.Margin = new System.Windows.Forms.Padding(6);
+            this.timeSelectionComboBox.Name = "timeSelectionComboBox";
+            this.timeSelectionComboBox.Size = new System.Drawing.Size(245, 28);
+            this.timeSelectionComboBox.TabIndex = 23;
+            // 
+            // resultDisplayLabel
+            // 
+            this.resultDisplayLabel.AutoSize = true;
+            this.resultDisplayLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.resultDisplayLabel.Location = new System.Drawing.Point(71, 517);
+            this.resultDisplayLabel.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.resultDisplayLabel.Name = "resultDisplayLabel";
+            this.resultDisplayLabel.Size = new System.Drawing.Size(66, 25);
+            this.resultDisplayLabel.TabIndex = 30;
+            this.resultDisplayLabel.Text = "Result";
+            // 
+            // thirdLabel
+            // 
+            this.thirdLabel.AutoSize = true;
+            this.thirdLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.thirdLabel.Location = new System.Drawing.Point(97, 266);
+            this.thirdLabel.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.thirdLabel.Name = "thirdLabel";
+            this.thirdLabel.Size = new System.Drawing.Size(95, 25);
+            this.thirdLabel.TabIndex = 29;
+            this.thirdLabel.Text = "Condition";
+            this.thirdLabel.TextAlign = System.Drawing.ContentAlignment.BottomRight;
+            // 
+            // secondLabel
+            // 
+            this.secondLabel.AutoSize = true;
+            this.secondLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.secondLabel.Location = new System.Drawing.Point(97, 201);
+            this.secondLabel.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.secondLabel.Name = "secondLabel";
+            this.secondLabel.Size = new System.Drawing.Size(95, 25);
+            this.secondLabel.TabIndex = 28;
+            this.secondLabel.Text = "Condition";
+            this.secondLabel.TextAlign = System.Drawing.ContentAlignment.BottomRight;
+            // 
+            // firstLabel
+            // 
+            this.firstLabel.AutoSize = true;
+            this.firstLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.firstLabel.Location = new System.Drawing.Point(97, 159);
+            this.firstLabel.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.firstLabel.Name = "firstLabel";
+            this.firstLabel.Size = new System.Drawing.Size(95, 25);
+            this.firstLabel.TabIndex = 27;
+            this.firstLabel.Text = "Condition";
+            this.firstLabel.TextAlign = System.Drawing.ContentAlignment.BottomRight;
+            // 
+            // calculateButton
+            // 
+            this.calculateButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.calculateButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.calculateButton.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.calculateButton.Location = new System.Drawing.Point(449, 375);
+            this.calculateButton.Margin = new System.Windows.Forms.Padding(6);
+            this.calculateButton.Name = "calculateButton";
+            this.calculateButton.Size = new System.Drawing.Size(98, 42);
+            this.calculateButton.TabIndex = 24;
+            this.calculateButton.Text = "Calculate";
+            this.calculateButton.UseVisualStyleBackColor = false;
+            this.calculateButton.Click += new System.EventHandler(this.calculateButton_Click);
+            // 
+            // thirdTextBox
+            // 
+            this.thirdTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.thirdTextBox.Location = new System.Drawing.Point(222, 263);
+            this.thirdTextBox.Margin = new System.Windows.Forms.Padding(6);
+            this.thirdTextBox.Name = "thirdTextBox";
+            this.thirdTextBox.Size = new System.Drawing.Size(246, 30);
+            this.thirdTextBox.TabIndex = 22;
+            // 
+            // secondTextBox
+            // 
+            this.secondTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.secondTextBox.Location = new System.Drawing.Point(223, 198);
+            this.secondTextBox.Margin = new System.Windows.Forms.Padding(6);
+            this.secondTextBox.Name = "secondTextBox";
+            this.secondTextBox.Size = new System.Drawing.Size(246, 30);
+            this.secondTextBox.TabIndex = 21;
+            // 
+            // firstTextBox
+            // 
+            this.firstTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.firstTextBox.Location = new System.Drawing.Point(222, 156);
+            this.firstTextBox.Margin = new System.Windows.Forms.Padding(6);
+            this.firstTextBox.Name = "firstTextBox";
+            this.firstTextBox.Size = new System.Drawing.Size(247, 30);
+            this.firstTextBox.TabIndex = 18;
+            // 
+            // whereConditionLabel
+            // 
+            this.whereConditionLabel.AutoSize = true;
+            this.whereConditionLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.whereConditionLabel.Location = new System.Drawing.Point(228, 121);
+            this.whereConditionLabel.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.whereConditionLabel.Name = "whereConditionLabel";
+            this.whereConditionLabel.Size = new System.Drawing.Size(0, 25);
+            this.whereConditionLabel.TabIndex = 20;
+            // 
+            // solveLabel
+            // 
+            this.solveLabel.AutoSize = true;
+            this.solveLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.solveLabel.Location = new System.Drawing.Point(96, 76);
+            this.solveLabel.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.solveLabel.Name = "solveLabel";
+            this.solveLabel.Size = new System.Drawing.Size(96, 25);
+            this.solveLabel.TabIndex = 19;
+            this.solveLabel.Text = "Solve For";
+            // 
+            // conditionSelectionComboBox
+            // 
+            this.conditionSelectionComboBox.BackColor = System.Drawing.Color.White;
+            this.conditionSelectionComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.conditionSelectionComboBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.conditionSelectionComboBox.FormattingEnabled = true;
+            this.conditionSelectionComboBox.Location = new System.Drawing.Point(222, 77);
+            this.conditionSelectionComboBox.Margin = new System.Windows.Forms.Padding(6);
+            this.conditionSelectionComboBox.Name = "conditionSelectionComboBox";
+            this.conditionSelectionComboBox.Size = new System.Drawing.Size(247, 28);
+            this.conditionSelectionComboBox.TabIndex = 17;
+            this.conditionSelectionComboBox.SelectedIndexChanged += new System.EventHandler(this.conditionSelectionComboBox_SelectedIndexChanged);
+            // 
+            // perYearLastLabel
+            // 
+            this.perYearLastLabel.AutoSize = true;
+            this.perYearLastLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.perYearLastLabel.ForeColor = System.Drawing.Color.Gray;
+            this.perYearLastLabel.Location = new System.Drawing.Point(157, 334);
+            this.perYearLastLabel.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.perYearLastLabel.Name = "perYearLastLabel";
+            this.perYearLastLabel.Size = new System.Drawing.Size(70, 20);
+            this.perYearLastLabel.TabIndex = 34;
+            this.perYearLastLabel.Text = "per year";
+            // 
+            // resultLabel
+            // 
+            this.resultLabel.BackColor = System.Drawing.Color.White;
+            this.resultLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.resultLabel.Location = new System.Drawing.Point(156, 468);
+            this.resultLabel.Name = "resultLabel";
+            this.resultLabel.Size = new System.Drawing.Size(391, 118);
+            this.resultLabel.TabIndex = 35;
+            this.resultLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(691, 660);
+            this.Controls.Add(this.resultLabel);
+            this.Controls.Add(this.perYearLastLabel);
+            this.Controls.Add(this.perYearLabel);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.timeSelectionComboBox);
+            this.Controls.Add(this.resultDisplayLabel);
+            this.Controls.Add(this.thirdLabel);
+            this.Controls.Add(this.secondLabel);
+            this.Controls.Add(this.firstLabel);
+            this.Controls.Add(this.calculateButton);
+            this.Controls.Add(this.thirdTextBox);
+            this.Controls.Add(this.secondTextBox);
+            this.Controls.Add(this.firstTextBox);
+            this.Controls.Add(this.whereConditionLabel);
+            this.Controls.Add(this.solveLabel);
+            this.Controls.Add(this.conditionSelectionComboBox);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label perYearLabel;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.ComboBox timeSelectionComboBox;
+        private System.Windows.Forms.Label resultDisplayLabel;
+        private System.Windows.Forms.Label thirdLabel;
+        private System.Windows.Forms.Label secondLabel;
+        private System.Windows.Forms.Label firstLabel;
+        private System.Windows.Forms.Button calculateButton;
+        private System.Windows.Forms.TextBox thirdTextBox;
+        private System.Windows.Forms.TextBox secondTextBox;
+        private System.Windows.Forms.TextBox firstTextBox;
+        private System.Windows.Forms.Label whereConditionLabel;
+        private System.Windows.Forms.Label solveLabel;
+        private System.Windows.Forms.ComboBox conditionSelectionComboBox;
+        private System.Windows.Forms.Label perYearLastLabel;
+        private System.Windows.Forms.Label resultLabel;
+    }
+}
+
---
+++ SimpleInterest Calculator/SimpleInterest Calculator/Form1.cs
@@ -0,0 +1,262 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SimpleInterest_Calculator
+{
+    public partial class Form1 : Form
+    {
+     string seletectedCondition;
+        string selectedTime;
+        double P;
+        double R;
+        double r;
+        double t;
+        double A;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            conditionSelectionComboBox.Items.AddRange(new string[] { "Total P+I (A)","Princiapal (P)","Rate (R)","Time Period (t)"});
+            timeSelectionComboBox.Items.AddRange(new string[] { "days (365/Yr)","days (360/Yr)","Weeks", "months","quarters","years" });
+            conditionSelectionComboBox.SelectedItem = "Total P+I (A)";
+            timeSelectionComboBox.SelectedItem = "years";
+            
+        }
+
+        private void conditionSelectionComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            seletectedCondition = conditionSelectionComboBox.SelectedItem.ToString();
+            if (seletectedCondition == "Total P+I (A)")
+            {
+                whereConditionLabel.Text = "Where: A = P(1+rt)";
+                firstLabel.Text = "Principal (P): $ ";
+                secondLabel.Text = "Rate (R): % ";
+                thirdLabel.Text = "Time(t) ";
+                thirdLabel.Location = new Point(97, 266);
+                thirdTextBox.Location = new Point(222, 263);
+                perYearLabel.Visible = true;
+                perYearLastLabel.Visible = false;
+                perYearLastLabel.Location = new Point(157, 334);
+                timeSelectionComboBox.Visible = true;
+                timeSelectionComboBox.Location = new Point(223, 305);
+
+            }
+            else if (seletectedCondition == "Princiapal (P)")
+            {
+                whereConditionLabel.Text = "Where: P = A / (1 + rt)";
+                firstLabel.Text = "Total P+I A:$ ";
+                secondLabel.Text = "Rate (R): % ";
+                thirdLabel.Text = "Time(t): ";
+                thirdLabel.Location = new Point(97, 266);
+                thirdTextBox.Location = new Point(222, 263);
+                perYearLabel.Visible = true;
+                perYearLastLabel.Visible = true;
+                perYearLastLabel.Location = new Point(157, 334);
+                timeSelectionComboBox.Visible = true;
+                timeSelectionComboBox.Location = new Point(223, 305);
+            }
+            else if (seletectedCondition == "Rate (R)")
+            {
+                whereConditionLabel.Text = "Where: r = (1/t)(A/P - 1)";
+                firstLabel.Text = "Total P+I A:$ ";
+                secondLabel.Text = "Principal (P): $ ";
+                thirdLabel.Text = "Time(t) ";
+                thirdLabel.Location = new Point(97, 239);
+                thirdTextBox.Location = new Point(223, 236);
+                perYearLabel.Visible = false;
+                perYearLastLabel.Visible = false;
+                perYearLastLabel.Location = new Point(157, 334);
+                timeSelectionComboBox.Visible = true;
+                timeSelectionComboBox.Location = new Point(223, 270);
+            }
+            else if (seletectedCondition == "Time Period (t)")
+            {
+                whereConditionLabel.Text = "Where: t = (1/r)(A/P - 1)";
+                firstLabel.Text = "Total P+I A:$";
+                secondLabel.Text = "Principal (P): $ ";
+                thirdLabel.Text = "Rate (R): %";
+                thirdLabel.Location = new Point(97, 239);
+                thirdTextBox.Location = new Point(223, 236);
+                perYearLabel.Visible = false;
+                perYearLastLabel.Visible = true;
+                perYearLastLabel.Location = new Point(157, 269);
+                timeSelectionComboBox.Visible = false;
+            }
+           
+        }
+
+        private void calculateButton_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(firstTextBox.Text) || string.IsNullOrEmpty(secondTextBox.Text) || string.IsNullOrEmpty(thirdTextBox.Text))
+            {
+                MessageBox.Show("Please fill all the required box");
+            }
+            else
+            {
+
+                selectedTime = timeSelectionComboBox.SelectedItem.ToString();
+
+                if (seletectedCondition == "Total P+I (A)")
+                {
+                    resultLabel.Text = GetTotalAccuredAmount().ToString("0.00");
+                }
+                else if (seletectedCondition == "Princiapal (P)")
+                {
+                    resultLabel.Text = GetPrincipalAmount().ToString("0.00");
+                }
+                else if (seletectedCondition == "Rate (R)")
+                {
+                    StringBuilder withRate = new StringBuilder();
+                    withRate.Append(GetRate().ToString("0.00"));
+                    withRate.Append(GetTimeText());
+                    resultLabel.Text = withRate.ToString();
+                }
+                else if (seletectedCondition == "Time Period (t)")
+                {
+                    StringBuilder withYear = new StringBuilder();
+                    withYear.Append(GetTimePeriod().ToString("0.00"));
+                    withYear.Append(" years");
+                    resultLabel.Text = withYear.ToString();
+                }
+            }
+            firstTextBox.Focus();
+        }
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            GetClear();
+        }
+        private void GetClear()
+        {
+            firstTextBox.Clear();
+            secondTextBox.Clear();
+            thirdTextBox.Clear();
+            resultLabel.Text = String.Empty;
+            firstTextBox.Focus();
+        }
+        private double GetTime()
+        {
+            if (selectedTime == "days (365/Yr)")
+            {
+                t = Convert.ToDouble(thirdTextBox.Text) / 365;
+            }
+            else if (selectedTime == "days (360/Yr)")
+            {
+                t = Convert.ToDouble(thirdTextBox.Text) / 360;
+            }
+            else if (selectedTime == "Weeks")
+            {
+                t = Convert.ToDouble(thirdTextBox.Text) / 52;
+            }
+            else if (selectedTime == "months")
+            {
+                t = Convert.ToDouble(thirdTextBox.Text) / 12;
+            }
+            else if (selectedTime == "quarters")
+            {
+                t = Convert.ToDouble(thirdTextBox.Text) / 4;
+            }
+            else
+            {
+                t = Convert.ToDouble(thirdTextBox.Text);
+            }
+            return t;
+        }
+        private string GetTimeText()
+        {
+            string timeText = "";
+            if (selectedTime == "days (365/Yr)")
+            {
+                timeText = "%/day";
+            }
+            else if (selectedTime == "days (360/Yr)")
+            {
+                timeText = "%/day";
+            }
+            else if (selectedTime == "Weeks")
+            {
+                timeText = "%/week";
+            }
+            else if (selectedTime == "months")
+            {
+                timeText = "%/month";
+            }
+            else if (selectedTime == "quarters")
+            {
+                timeText = "%/quarter";
+            }
+            else
+            {
+                timeText = "%/year";
+            }
+            return timeText;
+        }
+        private double GetTotalAccuredAmount()
+        {
+            R = Convert.ToDouble(secondTextBox.Text);
+            r = R / 100;
+            P = Convert.ToDouble(firstTextBox.Text);
+            t = GetTime();
+            A = 0;
+
+            
+            A = P * (1 + (r * t));
+            return A;
+        }
+        private double GetPrincipalAmount()
+        {
+            R = Convert.ToDouble(secondTextBox.Text);
+            r = R / 100;
+            P = 0;
+            t = GetTime();
+            A = Convert.ToDouble(firstTextBox.Text);
+            
+            P = A / (1 + (r * t));
+            return P;
+        }
+        private double GetRate()
+        {
+            
+            r = 0;
+            P = Convert.ToDouble(secondTextBox.Text);
+            t = Convert.ToDouble(thirdTextBox.Text);
+            A = Convert.ToDouble(firstTextBox.Text);
+            if (A>P)
+            {
+                r = (1 / t) * ((A / P) - 1);
+                R = r * 100;
+            }
+            else { MessageBox.Show("A must be greater than P");}
+
+           
+            return R;
+        }
+        private double GetTimePeriod()
+        {
+            R = Convert.ToDouble(thirdTextBox.Text);
+            r = R/100;
+            P = Convert.ToDouble(secondTextBox.Text);
+            A = Convert.ToDouble(firstTextBox.Text);
+
+            if (A>P)
+            {
+            t = (1/r)*((A/P)-1);
+            }
+            else
+            {
+                MessageBox.Show("A must be greater than P");
+            }
+            return t;
+        }
+    }
+}
