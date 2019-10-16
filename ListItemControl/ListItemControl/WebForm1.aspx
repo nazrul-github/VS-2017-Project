85f0539450ab32ee8e0cf45135b29730c0bf2fb4
Nazrul <nazrulgithub@gmail.com>
2019-10-16 21:00:05 +0600

    Update on printing values

---
+++ ListItemControl/ListItemControl/WebForm1.aspx
@@ -0,0 +1,26 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="ListItemControl.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:BulletedList ID="BulletedList1" runat="server">
+            </asp:BulletedList>
+            <br />
+            <asp:CheckBoxList ID="CheckBoxList1" runat="server">
+            </asp:CheckBoxList><br />
+            <asp:DropDownList ID="DropDownList1" runat="server">
+            </asp:DropDownList><br />
+            <asp:ListBox ID="ListBox1" runat="server" SelectionMode="Multiple"></asp:ListBox>
+            <asp:RadioButtonList ID="RadioButtonList1" runat="server">
+            </asp:RadioButtonList>
+            <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Button" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ ListItemControl/ListItemControl/WebForm1.aspx.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ListItemControl
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                PopulateList(DropDownList1);
+                PopulateList(CheckBoxList1);
+                PopulateList(RadioButtonList1);
+                PopulateList(ListBox1);
+            }
+        }
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            GetMultipleSelectedItem(ListBox1);
+            GetMultipleSelectedItem(CheckBoxList1);
+
+
+        }
+        private void PopulateList(ListControl listControl)
+        {
+            ListItem item1 = new ListItem("Item1", "1");
+            ListItem item2 = new ListItem("Item2", "2");
+            ListItem item3 = new ListItem("Item3", "3");
+            ListItem item4 = new ListItem("Item4", "4");
+
+            listControl.Items.AddRange(new ListItem[] { item1, item2, item3, item4 });
+
+        }
+        private void GetMultipleSelectedItem(ListControl listControl)
+        {
+            foreach (ListItem item in listControl.Items)
+            {
+                if (item.Selected)
+                {
+                    Response.Write($"Text: {item.Text}, Value:{item.Value}, Index: {listControl.Items.IndexOf(item)}<br/>");
+                }
+            }
+        }
+        private void GetSingleSelection(ListControl listControl)
+        {
+            if (listControl.SelectedIndex != -1)
+            {
+                ListItem item = new ListItem();
+                item = listControl.SelectedItem;
+                    Response.Write($"Text: {item.Text}, Value:{item.Value}, Index: {listControl.SelectedIndex.ToString()}");
+            }
+        }
+
+      
+    }
+}
