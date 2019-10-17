82944637402a15f937b68c0e1ac44f73378d1b86
Nazrul <nazrulgithub@gmail.com>
2019-10-17 21:13:17 +0600

    Created A Stored Prcoedure With Output Parameter to set the value to the database

---
+++ CallingAStoredProcedureWithOutputParameter/CallingAStoredProcedureWithOutputParameter/WebForm1.aspx
@@ -0,0 +1,37 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="CallingAStoredProcedureWithOutputParameter.WebForm1" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <table class="nav-justified" style="margin-bottom: 205px; border: 1px solid black; font-family: Arial">
+        <tr>
+            <td style="height: 20px; width: 379px;">Employee Name</td>
+            <td style="height: 20px">
+                <asp:TextBox ID="nameTextBox" runat="server"></asp:TextBox></td>
+
+        </tr>
+
+        <tr>
+            <td style="width: 379px">Gender</td>
+            <td><asp:DropDownList ID="genderDropDownList" runat="server" Height="16px" Width="141px">
+                <asp:ListItem>Male</asp:ListItem>
+                <asp:ListItem>Female</asp:ListItem>
+            </asp:DropDownList></td>
+        </tr>
+        <tr>
+            <td style="width: 379px">Salary</td>
+            <td><asp:TextBox ID="salaryTextBox" runat="server" Width="136px"></asp:TextBox></td>
+
+        </tr>
+        <tr>
+            <td style="width: 379px">
+                <asp:Button ID="submitButton" runat="server" Text="Submit" Width="209px" OnClick="submitButton_Click" /></td>
+
+
+        </tr>
+        <tr>
+            <td style="width: 379px">
+                <asp:Label ID="successLabel" runat="server"></asp:Label></td>
+
+
+        </tr>
+    </table>
+</asp:Content>
---
+++ CallingAStoredProcedureWithOutputParameter/CallingAStoredProcedureWithOutputParameter/WebForm1.aspx.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace CallingAStoredProcedureWithOutputParameter
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void submitButton_Click(object sender, EventArgs e)
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+            GetNewConnection(connectionString);
+
+        }
+
+        private void GetNewConnection(string connectionString)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand cmd = new SqlCommand();
+                cmd.CommandText = "spAddEmployee";
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@Name", nameTextBox.Text);
+                cmd.Parameters.AddWithValue("@Gender", genderDropDownList.SelectedValue);
+                cmd.Parameters.AddWithValue("@Salary",Convert.ToInt32(salaryTextBox.Text));
+                SqlParameter outParameter = new SqlParameter();
+                outParameter.DbType = DbType.Int32;
+                outParameter.Direction = ParameterDirection.Output;
+                outParameter.ParameterName = "@EmployeeId";
+                cmd.Parameters.Add(outParameter);
+                cmd.Connection = connection;
+                connection.Open();
+                cmd.ExecuteNonQuery();
+                string lblValue = outParameter.Value.ToString();
+                successLabel.Text = "Employee ID = " + lblValue;
+            }
+        }
+
+    }
+}
