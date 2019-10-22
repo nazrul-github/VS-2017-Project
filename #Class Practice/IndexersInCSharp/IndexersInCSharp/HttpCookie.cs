25aebae07496ebdf454f12c72a2d79be84e9f106
Nazrul <nazrulgithub@gmail.com>
2019-10-22 13:06:21 +0600

    Used indexer to set and get value to list & dictionary

---
+++ #Class Practice/IndexersInCSharp/IndexersInCSharp/HttpCookie.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace IndexersInCSharp
+{
+    public class HttpCookie
+    {
+        public readonly Dictionary<string, string> _dictionary;
+        public readonly List<string> aList;
+        public DateTime Expiry { get; set; }
+        
+
+
+        public HttpCookie()
+        {
+            _dictionary = new Dictionary<string, string>();
+            aList=new List<string>();
+        }
+
+       public string this[string key]
+        {
+            get => _dictionary[key];
+            set
+            {
+                _dictionary[key] = value;
+            }
+        }
+
+        public string this[int number]
+        {
+            get
+            {
+                return aList[number];
+            }
+            set { aList[number] = value; }
+        }
+    }
+}
